e434b724a91381b9b1728785c891f6bf3352f081
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2021-05-01 20:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         if(this.scrollY > 20)
-//             $(".navbar").addClass("sticky");
-//         else
-//             $(".navbar").removeClass("sticky")
-        
-//     });
-//     $(".menu-toggler").click(function(){
-//         $(this).toggleClass("active");
-//         $(".navbar-menu").toggleClass("active");
-//     })
-// });
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+            $(".goTop").fadeIn();
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+            $(".goTop").fadeOut();
+        }
+    });
+
+    $(".goTop").click(function(){
+        scroll(0,0)
+    });
+
+
+    $(".menu-toggler").click(function(){
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    })
+});
 
 
 
 
 
 
-$(".menu-toggler").click(function(){
-            $(this).toggleClass("active");
-            $(".navbar-menu").toggleClass("active");
-})
+// $(".menu-toggler").click(function(){
+//             $(this).toggleClass("active");
+//             $(".navbar-menu").toggleClass("active");
+// })
